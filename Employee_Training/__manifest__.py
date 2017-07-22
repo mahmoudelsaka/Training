cc45dda46d2032c20e56b89b55dfc72b46b6b233
mahmoudelsaka <mshaban2010.ms@gmail.com>
2017-07-22 20:45:27 +0200

    Training

---
+++ Employee_Training/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Employee's Training Management",
+
+
+
+    'author': "Mahmoud Elsaka",
+    'website': "http://www.elsaka.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale','mail','hr'],
+
+    # always loaded
+    'data': [
+
+        'security/training_security.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/sequence.xml',
+        'views/training_employee.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
