791edf054b06c080091f96cec7d6741b6a365c2a
mahmoudelsaka <mshaban2010.ms@gmail.com>
2019-04-02 00:51:20 +0200

    Update __openerp__.py

--- Employee_Training/__openerp__.py
+++ Employee_Training/__openerp__.py
@@ -9,7 +9,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resources',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
